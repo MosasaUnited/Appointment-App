7beba45b227c82e510b8cd038edf1faa2888c988
MosasaUnited <mostafa447@hotmail.com>
2024-03-25 20:13:52 +0200

    AddStylesConstantFile

---
+++ lib/core/theming/styles.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+;
+import 'colors.dart';
+
+abstract class Styles {
+  static TextStyle font24Black700Weight = TextStyle(
+    fontSize: 24.sp,
+    fontWeight: FontWeight.w700,
+    color: Colors.black
+    );
+
+  static TextStyle font32BlueBold = TextStyle(
+    fontSize: 32.sp,
+    fontWeight: FontWeight.bold,
+    color: AppColors.primaryColor,
+  );
+}
--- lib/features/onboarding/onboarding_screen.dart
+++ lib/features/onboarding/onboarding_screen.dart
@@ -1,12 +1,28 @@
+import 'package:appointment_app/features/onboarding/widgets/doc_logo_and_name.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('OnBoarding Screen', style: TextStyle(fontSize: 30)),
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.only(
+              top: 30.h,
+              bottom: 30.h,
+            ),
+            child: const Column(
+              children: [
+                DocLogoAndName(),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
